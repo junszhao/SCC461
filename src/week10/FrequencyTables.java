89fd3f4dc7d256faa42d4badcc095571290bb0b1
Matthew Rowe <m.rowe@lancaster.ac.uk>
2014-11-26 17:10:09 +0000

    Comitting code for week 10, part 3

---
+++ src/week10/FrequencyTables.java
@@ -0,0 +1,18 @@
+package week10;
+
+public class FrequencyTables {
+
+	public static void main(String[] args) {
+		
+		// you need to vary this
+		boolean filterStopWords = true;
+		
+		FrequencyTables frequencyTables = new FrequencyTables();
+		frequencyTables.computeTopTerm(filterStopWords);
+	}
+	
+	public String computeTopTerm(boolean filterStopWords) {
+		return null;
+	}
+	
+}
